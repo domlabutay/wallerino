e1e0166107b2f0596a96ff8bbed5ddc80e5dfce6
domlabutay <d.labutay@gmail.com>
2024-04-18 14:04:15 -0700

    initial commit

---
+++ src/main/java/com/dominiclabutay/personal/controllers/HomeController.java
@@ -0,0 +1,15 @@
+package com.dominiclabutay.personal.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+
+    @GetMapping("/")
+    public String index(Model model) {
+        model.addAttribute("test", "TEST");
+        return "index";
+    }
+}
---
+++ src/main/java/com/dominiclabutay/personal/services/ImageService.java
@@ -0,0 +1,8 @@
+package com.dominiclabutay.personal.services;
+
+import ij.IJ;
+import ij.ImagePlus;
+
+public class ImageService {
+    ImagePlus imp = IJ.openImage("/Users/dominiclabutay/Downloads/IMG_9067.png");
+}
