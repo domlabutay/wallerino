872d1f0695cd2c594162f8578a58c11b8b60c127
domlabutay <d.labutay@gmail.com>
2024-05-12 20:12:51 -0700

    website js backend and simple frontend

--- src/main/java/com/dominiclabutay/personal/controllers/HomeController.java
+++ src/main/java/com/dominiclabutay/personal/controllers/HomeController.java
@@ -1,8 +1,10 @@
 package com.dominiclabutay.personal.controllers;
 
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class HomeController {
--- src/main/java/com/dominiclabutay/personal/services/ImageService.java
+++
@@ -1,8 +0,0 @@
-package com.dominiclabutay.personal.services;
-
-import ij.IJ;
-import ij.ImagePlus;
-
-public class ImageService {
-    ImagePlus imp = IJ.openImage("/Users/dominiclabutay/Downloads/IMG_9067.png");
-}
